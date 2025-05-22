de3fce4242b9b5687cf1451c42c0c5ac22280eda
Miguel Cardoso <mac8.miguel@gmail.com>
2025-05-22 12:28:07 -0300

    Resolvido bug do Logout na página contato e o bug de acessar a página contato pelo dashboard.

--- interaction/route-controller/dashboard.js
+++ interaction/route-controller/dashboard.js
@@ -3,9 +3,9 @@ const links = Array.from(document.getElementsByClassName('dashboard-footer-link'
 console.log(links);
 
 links.forEach(link => {
-  link.addEventListener('click', (e) => {
-    e.preventDefault();
-    console.log(link.id, link.href);
-    window.location.href =`http://127.0.0.1:5500/pages/${link.id}.html`;
-  });
+  if (link.classList.contains('desactive')) {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+  }
 });
